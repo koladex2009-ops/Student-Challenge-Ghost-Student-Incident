d46dd069c21a63b581df02175caf6ab6068e71c7
Koladex <koladex2009@gmail.com>
2026-09-17 12:50:42 +0100

    Add dotenv for environment variable configuration

--- app.js
+++ app.js
@@ -196,21 +196,21 @@ A complete Postman request would be:
 
 */
 
-
+require('dotenv').config();
 
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const app = express();
 
-const port = 1212;
+const port = process.env.PORT 
 
 app.use(express.json());
 app.use(morgan("dev"));
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/techSchoolApp");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
